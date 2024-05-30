504a24232854e866356ce38bf9e3c80dedff6b1e
Ate329 <justin.he814@gmail.com>
2024-05-30 19:03:13 +0300

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  pkgs = import <nixpkgs> {};
+
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python311.withPackages (python-pkgs: [
+      python-pkgs.requests
+      python-pkgs.beautifulsoup4
+      # python-pkgs.bs4
+      python-pkgs.certifi
+      python-pkgs.charset-normalizer
+      python-pkgs.idna
+      python-pkgs.soupsieve
+      python-pkgs.urllib3
+      python-pkgs.cryptography
+    ]))
+  ];
+}
